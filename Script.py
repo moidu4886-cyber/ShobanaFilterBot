60493ff56f4de3c590c75009d808b63eeb3a7233
moidu4886-cyber <moidu4886@gmail.com>
2026-04-18 02:48:29 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -190,10 +190,6 @@ Added By - {}
 
     CUSTOM_FILE_CAPTION = """📂Fɪʟᴇɴᴀᴍᴇ : {file_name}
 FɪʟᴇSɪᴢᴇ : {file_size}
-
-⚠️ <b>This file will be deleted from here within 1 minute as it has copyright ... !!!</b>
-
-<b>കോപ്പിറൈറ്റ് ഉള്ളതുകൊണ്ട് ഫയൽ 1 മിനിറ്റിനുള്ളിൽ ഇവിടെനിന്നും ഡിലീറ്റ് ആകുന്നതാണ് അതുകൊണ്ട് ഇവിടെ നിന്നും മറ്റെവിടെക്കെങ്കിലും മാറ്റിയതിന് ശേഷം ഡൗൺലോഡ് ചെയ്യുക!</b>
 """
 
     
